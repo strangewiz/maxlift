24662fe3c73c8ecd49c7638a1243d9f9df8e4965
Justin Cohen <justinsc@gmail.com>
2025-11-27 09:28:50 -0500

    Initial Commit

---
+++ MaxLift/MaxLiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxLiftApp.swift
+//  MaxLift
+//
+//  Created by Justin Cohen on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MaxLiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
