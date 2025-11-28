3c0cebcfa2de0fbee8f7a98c46671894241cbb5f
Justin Cohen <justinsc@gmail.com>
2025-11-28 00:41:28 -0500

    colors

---
+++ MaxLift/Theme/Color+Theme.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension Color {
+    static let appBackground = Color("CustomAppBackground")
+    static let cardBackground = Color("CustomCardBackground")
+    static let appAccent = Color("CustomAccent")
+    static let textSecondary = Color("CustomTextSecondary")
+}
